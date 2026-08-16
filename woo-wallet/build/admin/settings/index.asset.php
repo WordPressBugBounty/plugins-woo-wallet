9b459c1f04ca47b4b3cf14cd303ff3a8b7d309d9
Sean Murphy <sean@iamseanmurphy.com>
2026-08-16 06:55:45 +0000

    1.6.12

--- woo-wallet/build/admin/settings/index.asset.php
+++ woo-wallet/build/admin/settings/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '19b8e7699c7f0364ff95');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '11a47509431bfed6dff5');
